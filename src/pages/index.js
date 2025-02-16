ff0ecceb66a6e9e8c9c8c3ee884caa6991126d3e
Alexandria Cox <alexacox.atc@gmail.com>
2025-02-16 18:27:03 -0500

    feat: Add page components and barrel exports

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { default as Home } from './Home';
+import { default as Gallery } from './Gallery';
+import { default as Profile } from './Profile';
+import { default as NotFound } from './NotFound';
+import { default as Login } from './Auth/Login';
+import { default as Register } from './Auth/Register';
+
+export { Home, Gallery, Profile, NotFound, Login, Register };
