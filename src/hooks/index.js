b15e2ba80854ad42fcc15f7a91a238458dc38a09
Alexandria Cox <alexacox.atc@gmail.com>
2025-05-04 04:43:18 -0400

    feat: Add section observer hook and scrolling features

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,7 @@
 export * from './useClickHandler';
 export * from './useDeleteHandler';
 export { default as useImageErrorHandler } from './useImageErrorHandler';
+export * from './useImageLoader';
 export * from './useKeyboardHandler';
 export * from './useNavigationHandler';
-export * from './useImageLoader';
+export * from './useSectionObserver';
